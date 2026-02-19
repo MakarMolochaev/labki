31e88e4d2bd09abf8e00b6874f53b23fc46d4666
Makar Molochaev <Makara1006@gmail.com>
2026-02-19 19:15:41 +0300

    split to h & cpp

---
+++ 1/ccomplexvector.cpp
@@ -0,0 +1,161 @@
+#include "CComplexVector.h"
+#include <fstream>
+#include <stdexcept>
+
+// CComplexVector implementation
+CComplexVector::CComplexVector(size_t size) {
+    data_ = std::vector<std::pair<double, double>>(size);
+}
+
+CComplexVector::CComplexVector() = default;
+
+CComplexVector::CComplexVector(const std::vector<double>& data) {
+    if (data.size() % 2 != 0) {
+        throw std::runtime_error("wrong size");
+    }
+    for (size_t i = 0; i < data.size(); i += 2) {
+        data_.push_back(std::pair<double,double>(data[i], data[i+1]));
+    }
+}
+
+CComplexVector::CComplexVector(const CComplexVector& other) {
+    for (auto p : other.data_) {
+        data_.push_back(std::pair<double,double>(p.first, p.second));
+    }
+}
+
+CComplexVector::CComplexVector(const CComplexVector&& other) noexcept 
+    : data_(std::move(other.data_)) {}
+
+CComplexVector& CComplexVector::operator=(const CComplexVector& other) {
+    if (this != &other) {
+        data_ = other.data_;
+    }
+    return *this;
+}
+
+CComplexVector& CComplexVector::operator=(CComplexVector&& other) noexcept {
+    if (this != &other) {
+        data_ = std::move(other.data_);
+    }
+    return *this;
+}
+
+// CComplexVector0 implementation
+CComplexVector0::CComplexVector0(const CComplexVector& other) 
+    : CComplexVector(other) {}
+
+CComplexVector0::CComplexVector0(const CComplexVector&& other) noexcept 
+    : CComplexVector(other) {}
+
+CComplexVector0::~CComplexVector0() = default;
+
+CComplexVector0 CComplexVector0::operator+(CComplexVector0& other) const {
+    if (data_.size() != other.data_.size()) {
+        throw std::runtime_error("vectors must have same size");
+    }
+
+    CComplexVector0 result(data_.size());
+
+    for (size_t i = 0; i < data_.size(); i++) {
+        result.data_[i].first = data_[i].first + other.data_[i].first; 
+        result.data_[i].second = data_[i].second + other.data_[i].second; 
+    }
+
+    return result;
+}
+
+CComplexVector0 CComplexVector0::operator-(CComplexVector0& other) const {
+    if (data_.size() != other.data_.size()) {
+        throw std::runtime_error("vectors must have same size");
+    }
+
+    CComplexVector0 result(data_.size());
+
+    for (size_t i = 0; i < data_.size(); i++) {
+        result.data_[i].first = data_[i].first - other.data_[i].first; 
+        result.data_[i].second = data_[i].second - other.data_[i].second; 
+    }
+
+    return result;
+}
+
+double CComplexVector0::Dot(const CComplexVector0& other) const {
+    double result = 0;
+    
+    for (size_t i = 0; i < data_.size(); i++) {
+        result += (data_[i].first * other.data_[i].first);
+        result -= (data_[i].second * other.data_[i].second); 
+    }
+
+    return result;
+}
+
+const void CComplexVector0::Output(std::string filename) {
+    std::ofstream file(filename, std::ios::out);
+    for (size_t i = 0; i < data_.size(); i++) {
+        file << "(" << data_[i].first << " + " << data_[i].second << "i)";
+        if (i < data_.size() - 1) {
+            file << ", ";
+        }
+    }
+    file.close();
+}
+
+// CComplexVector1 implementation
+CComplexVector1::CComplexVector1(const CComplexVector& other) 
+    : CComplexVector(other) {}
+
+CComplexVector1::CComplexVector1(const CComplexVector&& other) noexcept 
+    : CComplexVector(other) {}
+
+CComplexVector1::~CComplexVector1() = default;
+
+CComplexVector1 CComplexVector1::operator+(CComplexVector1& other) const {
+    if (data_.size() != other.data_.size()) {
+        throw std::runtime_error("vectors must have same size");
+    }
+
+    CComplexVector1 result(data_.size());
+
+    for (size_t i = 0; i < data_.size(); i++) {
+        result.data_[i].first = data_[i].first + other.data_[i].first; 
+        result.data_[i].second = data_[i].second + other.data_[i].second; 
+    }
+
+    return result;
+}
+
+CComplexVector1 CComplexVector1::operator-(CComplexVector1& other) const {
+    if (data_.size() != other.data_.size()) {
+        throw std::runtime_error("vectors must have same size");
+    }
+
+    CComplexVector1 result(data_.size());
+
+    for (size_t i = 0; i < data_.size(); i++) {
+        result.data_[i].first = data_[i].first - other.data_[i].first; 
+        result.data_[i].second = data_[i].second - other.data_[i].second; 
+    }
+
+    return result;
+}
+
+double CComplexVector1::Dot(const CComplexVector1& other) const {
+    double result = 0;
+    
+    for (size_t i = 0; i < data_.size(); i++) {
+        result += (data_[i].first * other.data_[i].first);
+        result -= (data_[i].second * other.data_[i].second); 
+    }
+
+    return result;
+}
+
+const void CComplexVector1::Output(std::string filename) {
+    std::ofstream file(filename, std::ios::out);
+    for (auto p : data_) {
+        file << "(" << p.first << " + " << p.second << "i)\n";
+    }
+    file.close();
+}
--- 1/ccomplexvector.h
+++ 1/ccomplexvector.h
@@ -1,172 +1,56 @@
+#pragma once
+
 #include <string>
-#include <fstream>
 #include <vector>
+#include <utility>
 
 class CComplexVector {
 protected:
     std::vector<std::pair<double, double>> data_;
+
 public:
-    CComplexVector(size_t size) {
-        data_ = std::vector<std::pair<double, double>>(size);
-    }
-
-    CComplexVector() = default;
-
-    CComplexVector(const std::vector<double>& data) {
-        if (data.size() % 2 != 0) {
-            throw std::runtime_error("wrong size");
-        }
-        for (int i = 0; i < data.size(); i += 2) {
-            data_.push_back(std::pair<double,double>(data[i], data[i+1]));
-        }
-    }
-
-    CComplexVector(const CComplexVector& other) {
-        for (auto p : other.data_) {
-            data_.push_back(std::pair<double,double>(p.first, p.second));
-        }
-    }
-
-    CComplexVector(const CComplexVector&& other) noexcept : data_(std::move(other.data_)) {}
-
-    CComplexVector& operator=(const CComplexVector& other) {
-        if (this != &other) {
-            data_ = other.data_;
-        }
-        return *this;
-    }
-
-    CComplexVector& operator=(CComplexVector&& other) noexcept {
-        if (this != &other) {
-            data_ = std::move(other.data_);
-        }
-        return *this;
-    }
-
+    CComplexVector(size_t size);
+    CComplexVector();
+    CComplexVector(const std::vector<double>& data);
+    CComplexVector(const CComplexVector& other);
+    CComplexVector(const CComplexVector&& other) noexcept;
+    
+    CComplexVector& operator=(const CComplexVector& other);
+    CComplexVector& operator=(CComplexVector&& other) noexcept;
+    
     virtual ~CComplexVector() = default;
-
-    const virtual void Output(std::string filename) = 0;
+    
+    virtual const void Output(std::string filename) = 0;
 };
 
 // row output
 class CComplexVector0 : public CComplexVector {
 public:
     using CComplexVector::CComplexVector;
-
-    CComplexVector0(const CComplexVector& other) : CComplexVector(other){}
-    CComplexVector0(const CComplexVector&& other) noexcept : CComplexVector(other){}
     
-    ~CComplexVector0() override = default;
-
-    CComplexVector0 operator+(CComplexVector0& other) const {
-        if (data_.size() != other.data_.size()) {
-            throw std::runtime_error("vectors must have same size");
-        }
-
-        CComplexVector0 result(data_.size());
-
-        for (int i = 0; i < data_.size(); i++) {
-            result.data_[i].first = data_[i].first + other.data_[i].first; 
-            result.data_[i].second = data_[i].second + other.data_[i].second; 
-        }
-
-        return result;
-    }
-
-    CComplexVector0 operator-(CComplexVector0& other) const {
-        if (data_.size() != other.data_.size()) {
-            throw std::runtime_error("vectors must have same size");
-        }
-
-        CComplexVector0 result(data_.size());
-
-        for (int i = 0; i < data_.size(); i++) {
-            result.data_[i].first = data_[i].first - other.data_[i].first; 
-            result.data_[i].second = data_[i].second - other.data_[i].second; 
-        }
-
-        return result;
-    }
-
-    double Dot(const CComplexVector0& other) const {
-        double result = 0;
-        
-        for (int i = 0; i < data_.size(); i++) {
-            result += (data_[i].first * other.data_[i].first);
-            result -= (data_[i].second * other.data_[i].second); 
-        }
-
-        return result;
-    }
+    CComplexVector0(const CComplexVector& other);
+    CComplexVector0(const CComplexVector&& other) noexcept;
     
-    const void Output(std::string filename) override {
-        std::ofstream file(filename, std::ios::out);
-        for (int i = 0; i < data_.size(); i++) {
-            file << "(" << data_[i].first << " + " << data_[i].second << "i)";
-            if (i < data_.size() - 1) {
-                file << ", ";
-            }
-        }
-        file.close();
-    }
+    ~CComplexVector0() override;
+    
+    CComplexVector0 operator+(CComplexVector0& other) const;
+    CComplexVector0 operator-(CComplexVector0& other) const;
+    double Dot(const CComplexVector0& other) const;
+    const void Output(std::string filename) override;
 };
 
 // column output
 class CComplexVector1 : public CComplexVector {
 public:
     using CComplexVector::CComplexVector;
-
-    CComplexVector1(const CComplexVector& other) : CComplexVector(other){}
-    CComplexVector1(const CComplexVector&& other) noexcept : CComplexVector(other){}
-
-    ~CComplexVector1() override = default;
-
-    CComplexVector1 operator+(CComplexVector1& other) const {
-        if (data_.size() != other.data_.size()) {
-            throw std::runtime_error("vectors must have same size");
-        }
-
-        CComplexVector1 result(data_.size());
-
-        for (int i = 0; i < data_.size(); i++) {
-            result.data_[i].first = data_[i].first + other.data_[i].first; 
-            result.data_[i].second = data_[i].second + other.data_[i].second; 
-        }
-
-        return result;
-    }
-
-    CComplexVector1 operator-(CComplexVector1& other) const {
-        if (data_.size() != other.data_.size()) {
-            throw std::runtime_error("vectors must have same size");
-        }
-
-        CComplexVector1 result(data_.size());
-
-        for (int i = 0; i < data_.size(); i++) {
-            result.data_[i].first = data_[i].first - other.data_[i].first; 
-            result.data_[i].second = data_[i].second - other.data_[i].second; 
-        }
-
-        return result;
-    }
-
-    double Dot(const CComplexVector1& other) const {
-        double result = 0;
-        
-        for (int i = 0; i < data_.size(); i++) {
-            result += (data_[i].first * other.data_[i].first);
-            result -= (data_[i].second * other.data_[i].second); 
-        }
-
-        return result;
-    }
-
-    const void Output(std::string filename) override {
-        std::ofstream file(filename, std::ios::out);
-        for (auto p : data_) {
-            file << "(" << p.first << " + " << p.second << "i)\n";
-        }
-        file.close();
-    }
+    
+    CComplexVector1(const CComplexVector& other);
+    CComplexVector1(const CComplexVector&& other) noexcept;
+    
+    ~CComplexVector1() override;
+    
+    CComplexVector1 operator+(CComplexVector1& other) const;
+    CComplexVector1 operator-(CComplexVector1& other) const;
+    double Dot(const CComplexVector1& other) const;
+    const void Output(std::string filename) override;
 };
